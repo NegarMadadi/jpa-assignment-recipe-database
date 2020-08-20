7fedf8ebb3ce3dc05f547443fbb01d7f91c3e55f
Negar <negar.myeah@gmail.com>
2020-08-20 21:45:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/negar/jpaassignmentrecipedatabase/JpaAssignmentRecipeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.negar.jpaassignmentrecipedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentRecipeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAssignmentRecipeDatabaseApplication.class, args);
+	}
+
+}
